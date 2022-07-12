febf3df17977dc8f9042461359fa3f464a617b20
Thiago Minguim <Chazy249@gmail.com>
2022-07-12 18:12:52 -0300

    finish initial settings

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles'
+
+const Home = () => {
+  return (
+    <Container>
+      <span>home</span>
+    </Container>
+  )
+}
+
+export default Home
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import * as path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '@/public', replacement: path.resolve(__dirname, '/public') },
+      { find: '@', replacement: path.resolve(__dirname, '/src') }
+    ]
+  },
+
   plugins: [react()]
 })
