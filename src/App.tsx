c9ae3ef4617b2932b209e5f4c863b45c2414f2f5
Thiago Minguim <Chazy249@gmail.com>
2022-07-13 00:10:16 -0300

    Work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { ThemeProvider } from 'styled-components'
 import Home from './pages/Home'
 import GlobalStyle from './styles/global'
+import theme from './styles/theme'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Home />
-    </>
+    </ThemeProvider>
   )
 }
 
